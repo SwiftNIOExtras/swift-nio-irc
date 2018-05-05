ce20231ac3852186de8ef9f32a2ec50fe46c5fe9
Helge Hess <me@helgehess.eu>
2018-05-05 12:14:57 +0200

    Refer to NIO master branch
    ... for testing against upstream master.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", 
-                 from: "1.5.1")
+                 .branch("master")),
     ],
     targets: [
         .target(name: "NIOIRC", dependencies: [ "NIO"    ]),
