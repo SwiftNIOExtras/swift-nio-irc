80261d2c0919621842aeaee6569c68afb7ab40c7
Helge Heß <devteam@zeezide.de>
2021-03-08 15:57:42 +0100

    Simplify the ByteBuffer extension
    As per https://github.com/SwiftNIOExtras/swift-nio-irc/issues/3,
    and after discussion w/ @weissi, just using a String write should
    be fine and won't result in allocations. Xlnt.

--- Sources/NIOIRC/Helpers/ByteBufferExtras.swift
+++ Sources/NIOIRC/Helpers/ByteBufferExtras.swift
@@ -14,69 +14,30 @@
 
 import struct NIO.ByteBuffer
 
-fileprivate extension BinaryInteger {
+public extension ByteBuffer {
+  // This looks expensive, but isn't. As per @weissi String's store up to 15
+  // bytes inline, no alloc necessary.
   
-  var numberOfDecimalDigits : Int {
-    @inline(__always) get {
-      var value = self
-      var count = 0
-      
-      repeat {
-        value /= 10
-        count += 1
-      }
-      while value != 0
-      
-      return count
-    }
-  }
-}
-
-extension ByteBuffer {
-  
+  /**
+   * Write an Integer as an ASCII String.
+   */
+  @inlinable
   @discardableResult
-  public mutating func write<T: SignedInteger>(integerAsString integer: T,
-                                               as: T.Type = T.self) -> Int
+  mutating func write<T: SignedInteger>(integerAsString integer: T,
+                                        as: T.Type = T.self) -> Int
   {
-    let bytesWritten = set(integerAsString: integer, at: self.writerIndex)
-    moveWriterIndex(forwardBy: bytesWritten)
-    return Int(bytesWritten)
+    return self.writeString(String(integer, radix: 10))
   }
 
+  /**
+   * Set an Integer as an ASCII String.
+   */
+  @inlinable
   @discardableResult
-  public mutating func set<T: SignedInteger>(integerAsString integer: T,
-                                             at index: Int,
-                                             as: T.Type = T.self) -> Int
+  mutating func set<T: SignedInteger>(integerAsString integer: T,
+                                      at index: Int,
+                                      as: T.Type = T.self) -> Int
   {
-    let charCount = integer.numberOfDecimalDigits + (integer < 0 ? 1 : 0)
-    let avail     = capacity - index
-    
-    if avail < charCount {
-      reserveCapacity(capacity + (charCount - avail))
-    }
-
-    self.withVeryUnsafeBytes { rbpp in
-      let mrbpp  = UnsafeMutableRawBufferPointer(mutating: rbpp)
-      let base   = mrbpp.baseAddress!.assumingMemoryBound(to: UInt8.self)
-                        .advanced(by: index)
-      var cursor = base.advanced(by: charCount)
-      
-      let c0 : T = 48
-      var negativeAbsoluteValue = integer < 0 ? integer : -integer
-      repeat {
-        cursor -= 1
-        cursor.pointee = UInt8(c0 - (negativeAbsoluteValue % 10))
-        negativeAbsoluteValue /= 10;
-      }
-      while negativeAbsoluteValue != 0
-      
-      if integer < 0 {
-        cursor -= 1
-        cursor.pointee = 45 // -
-      }
-
-    }
-    
-    return charCount
+    return self.setString(String(integer, radix: 10), at: index)
   }
 }
